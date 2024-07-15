48f49a733e4a20395676e6e3df5bf5933ac6d2e6
meakashdash <akashdash279@gmail.com>
2024-07-16 00:26:40 +0530

    changed server.js

--- server.js
+++ server.js
@@ -1,41 +1,32 @@
-const express=require('express')
-const next=require('next')
-const {createProxyMiddleware} = require('http-proxy-middleware')
+const express = require('express');
+const next = require('next');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-//check if we are in dev mode or production mode
-const dev=process.env.NODE_ENV !== 'production'
-const app=next({dev})
-const handle=app.getRequestHandler()
+app.prepare().then(() => {
+    const server = express();
 
+    // Apply the proxy middleware for both development and production modes
+    server.use('/api', createProxyMiddleware({
+        target: "http://13.232.228.155:8000",
+        changeOrigin: true,
+    }));
 
+    // Handle all other requests
+    server.all('*', (req, res) => {
+        return handle(req, res);
+    });
 
-
-app.prepare().then(()=>{
-    //create a express server
-    const server=express();
-    //apply when we are in dev mode
-    if(dev){
-        //use this middleware when we hit /api call or backend call then create a proxy server on this target
-        //to make it as a same origin
-        server.use('/api',createProxyMiddleware({
-            target:"http://13.232.228.155:8000",
-            changeOrigin:true,
-        }))
-    }
-
-    //when we get a request then we have to handle the request and response
-    server.all('*',(req,res)=>{
-        return handle(req,res)
-    })
-
-    //run the server
-    server.listen(3000,(err)=>{
-        if(err){
-            throw err
+    // Start the server
+    server.listen(3000, (err) => {
+        if (err) {
+            throw err;
         }
-        console.log("Ready on http://13.232.228.155:3000")
-    })
-}).catch(err=>{
-    console.log('Error:',err)
-})
+        console.log("Ready on http://13.232.228.155:3000");
+    });
+}).catch(err => {
+    console.log('Error:', err);
+});
